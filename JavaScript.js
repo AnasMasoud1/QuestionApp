403619dacb01160bae803f803d905cfcce907f26
Anas Masoud <anas.masoud9@gmail.com>
2023-11-06 13:19:39 +0200

    UploadApp

---
+++ JavaScript.js
@@ -0,0 +1,274 @@
+﻿//let x = document.getElementById('start').onclick = function GoTest() {
+//    location.href = "TestLevel.html";
+//}
+//function GoTest() {
+//    location.replace("TestLevel.html");
+//}
+/*var jtiq1 = document.getElementById('tiq1').val ;*/
+
+
+
+// Test Script
+var xx = document.getElementById("hone");
+
+
+
+
+
+    //    if (jtiq1 == jtlq1) {
+    //        score = score + 2;
+    //    }
+    //  function tCheckInput2() {
+    //        if (jtiq2 == jtlq2) {
+    //            score = score + 2;
+    //        }
+    //    }
+
+    //        function tCheckInput3() {
+    //            if (jtiq3 == jtlq3) {
+    //                score = score + 2;
+    //            }
+    //        }
+    //    var jtlq4 = document.getElementById('tlq4');
+    //  jtlq4 = 1;
+    //            function tCheckInput4() {
+    //                if (jtiq4 == jtlq4) {
+    //                    score = score + 2;
+    //                }
+    //            }
+
+
+    //                function tCheckInput5() {
+    //                    if (jtiq5 == jtlq5) {
+    //                        score = score + 2;
+    //                    }
+
+    //}
+
+
+    //var jtsubmit = document.getElementById('tsubmit');
+    //jtsubmit.onclick = function checkScore() {
+   
+
+
+// Hard Script 
+
+//var randomNo1 = Math.round(Math.random() * 10) + 5;
+//var randomNo2 = Math.round(Math.random() * 5) + 1;
+//var randomNo3 = Math.floor(Math.random() * 5) + 1;
+
+//document.getElementById('hlq1').innerHTML = randomNo1 + "*" + randomNo2 + "-" + randomNo3;
+//var jhlq1 = randomNo1 * randomNo2 - randomNo3;
+//document.getElementById('hlq2').innerHTML = randomNo1 + "*" + randomNo2 + "-" + randomNo3;
+//var jhlq2 = randomNo1 * randomNo2 - randomNo3;
+//document.getElementById('hlq3').innerHTML = randomNo1 + "*" + randomNo2 + "-" + randomNo3;
+//var jhlq3 = randomNo1 * randomNo2 - randomNo3;
+//document.getElementById('hlq4').innerHTML = randomNo1 + "*" + randomNo2 + "-" + randomNo3;
+//var jhlq4 = randomNo1 * randomNo2 - randomNo3;
+//document.getElementById('hlq5').innerHTML = randomNo1 + "*" + randomNo2 + "-" + randomNo3;
+//var jhlq5 = randomNo1 * randomNo2 - randomNo3;
+//document.getElementById('hlq6').innerHTML = randomNo1 + "*" + randomNo2 + "-" + randomNo3;
+//var jhlq6 = randomNo1 * randomNo2 - randomNo3;
+//document.getElementById('hlq7').innerHTML = randomNo1 + "*" + randomNo2 + "-" + randomNo3;
+//var jhlq7 = randomNo1 * randomNo2 - randomNo3;
+//document.getElementById('hlq8').innerHTML = randomNo1 + "*" + randomNo2 + "-" + randomNo3;
+//var jhlq8 = randomNo1 * randomNo2 - randomNo3;
+//document.getElementById('hlq9').innerHTML = randomNo1 + "*" + randomNo2 + "-" + randomNo3;
+//var jhlq9 = randomNo1 * randomNo2 - randomNo3;
+//document.getElementById('hlq10').innerHTML = randomNo1 + "*" + randomNo2 + "-" + randomNo3;
+//var jhlq10 = randomNo1 * randomNo2 - randomNo3;
+//document.getElementById('hlq11').innerHTML = randomNo1 + "*" + randomNo2 + "-" + randomNo3;
+//var jhlq11 = randomNo1 * randomNo2 - randomNo3;
+//document.getElementById('hlq12').innerHTML = randomNo1 + "*" + randomNo2 + "-" + randomNo3;
+//var jhlq12 = randomNo1 * randomNo2 - randomNo3;
+//document.getElementById('hlq13').innerHTML = randomNo1 + "*" + randomNo2 + "-" + randomNo3;
+//var jhlq13 = randomNo1 * randomNo2 - randomNo3;
+//document.getElementById('hlq14').innerHTML = randomNo1 + "*" + randomNo2 + "-" + randomNo3;
+//var jhlq14 = randomNo1 * randomNo2 - randomNo3;
+//document.getElementById('hlq15').innerHTML = randomNo1 + "*" + randomNo2 + "-" + randomNo3;
+//var jhlq15 = randomNo1 * randomNo2 - randomNo3;
+//document.getElementById('hlq16').innerHTML = randomNo1 + "*" + randomNo2 + "-" + randomNo3;
+//var jhlq16= randomNo1 * randomNo2 - randomNo3;
+//document.getElementById('hlq17').innerHTML = randomNo1 + "*" + randomNo2 + "-" + randomNo3;
+//var jhlq17 = randomNo1 * randomNo2 - randomNo3;
+//document.getElementById('hlq18').innerHTML = randomNo1 + "*" + randomNo2 + "-" + randomNo3;
+//var jhlq18 = randomNo1 * randomNo2 - randomNo3;
+//document.getElementById('hlq19').innerHTML = randomNo1 + "*" + randomNo2 + "-" + randomNo3;
+//var jhlq19 = randomNo1 * randomNo2 - randomNo3;
+//document.getElementById('hlq20').innerHTML = randomNo1 + "*" + randomNo2 + "-" + randomNo3;
+//var jhlq20 = randomNo1 * randomNo2 - randomNo3;
+//document.getElementById('hlq21').innerHTML = randomNo1 + "*" + randomNo2 + "-" + randomNo3;
+//var jhlq21 = randomNo1 * randomNo2 - randomNo3;
+//document.getElementById('hlq22').innerHTML = randomNo1 + "*" + randomNo2 + "-" + randomNo3;
+//var jhlq22 = randomNo1 * randomNo2 - randomNo3;
+//document.getElementById('hlq23').innerHTML = randomNo1 + "*" + randomNo2 + "-" + randomNo3;
+//var jhlq23 = randomNo1 * randomNo2 - randomNo3;
+//document.getElementById('hlq24').innerHTML = randomNo1 + "*" + randomNo2 + "-" + randomNo3;
+//var jhlq24 = randomNo1 * randomNo2 - randomNo3;
+//document.getElementById('hlq25').innerHTML = randomNo1 + "*" + randomNo2 + "-" + randomNo3;
+//var jhlq25 = randomNo1 * randomNo2 - randomNo3;
+
+
+
+
+//const Game = () => {
+//    var jhiq1  = document.getElementById("hiq1 ").value;
+//    var jhiq2  = document.getElementById("hiq2 ").value;
+//    var jhiq3  = document.getElementById("hiq3 ").value;
+//    var jhiq4  = document.getElementById("hiq4 ").value;
+//    var jhiq5  = document.getElementById("hiq5 ").value;
+//    var jhiq6  = document.getElementById("hiq6 ").value;
+//    var jhiq7  = document.getElementById("hiq7 ").value;
+//    var jhiq8  = document.getElementById("hiq8 ").value;
+//    var jhiq9  = document.getElementById("hiq9 ").value;
+//    var jhiq10 = document.getElementById("hiq10").value;
+//    var jhiq11 = document.getElementById("hiq11").value;
+//    var jhiq12 = document.getElementById("hiq12").value;
+//    var jhiq13 = document.getElementById("hiq13").value;
+//    var jhiq14 = document.getElementById("hiq14").value;
+//    var jhiq15 = document.getElementById("hiq15").value;
+//    var jhiq16 = document.getElementById("hiq16").value;
+//    var jhiq17 = document.getElementById("hiq17").value;
+//    var jhiq18 = document.getElementById("hiq18").value;
+//    var jhiq19 = document.getElementById("hiq19").value;
+//    var jhiq20 = document.getElementById("hiq20").value;
+//    var jhiq21 = document.getElementById("hiq21").value;
+//    var jhiq22 = document.getElementById("hiq22").value;
+//    var jhiq23 = document.getElementById("hiq23").value;
+//    var jhiq24 = document.getElementById("hiq24").value;
+//    var jhiq25 = document.getElementById("hiq25").value;
+    
+//    if (jhiq1 == jhlq1) {
+//        score = score + 2;
+
+//    }
+//    if (jhiq2 == jhlq2) {
+//        score = score + 2;
+
+//    }
+//    if (jhiq3 == jhlq3) {
+//        score = score + 2;
+
+//    }
+//    if (jhiq4 == jhlq4) {
+//        score = score + 2;
+
+//    }
+//    if (jhiq5 == jhlq5) {
+//        score = score + 2;
+
+//    }
+//    if (jhiq6 == jhlq6) {
+//        score = score + 2;
+
+//    }
+//    if (jhiq7 == jhlq7) {
+//        score = score + 2;
+
+//    }
+//    if (jhiq8 == jhlq8) {
+//        score = score + 2;
+
+//    }
+//    if (jhiq9 == jhlq9) {
+//        score = score + 2;
+
+//    }
+//    if (jhiq10 == jhlq10) {
+//        score = score + 2;
+
+//    }
+//    if (jhiq11 == jhlq11) {
+//        score = score + 2;
+
+//    }
+//    if (jhiq12 == jhlq12) {
+//        score = score + 2;
+
+//    }
+//    if (jhiq13 == jhlq13) {
+//        score = score + 2;
+
+//    }
+//    if (jhiq14 == jhlq14) {
+//        score = score + 2;
+
+//    }
+//    if (jhiq15 == jhlq15) {
+//        score = score + 2;
+
+//    }
+//    if (jhiq16 == jhlq16) {
+//        score = score + 2;
+
+//    }
+//    if (jhiq17 == jhlq17) {
+//        score = score + 2;
+
+//    }
+//    if (jhiq18 == jhlq18) {
+//        score = score + 2;
+
+//    }
+//    if (jhiq19 == jhlq19) {
+//        score = score + 2;
+
+//    }
+//    if (jhiq20 == jhlq20) {
+//        score = score + 2;
+
+//    }
+//    if (jhiq21 == jhlq21) {
+//        score = score + 2;
+
+//    }
+//    if (jhiq22 == jhlq22) {
+//        score = score + 2;
+
+//    }
+//    if (jhiq23 == jhlq23) {
+//        score = score + 2;
+
+//    }
+//    if (jhiq24 == jhlq24) {
+//        score = score + 2;
+
+//    }
+//    if (jhiq25 == jhlq25) {
+//        score = score + 2;
+
+//    }
+    
+
+//    if (score <= 5) {
+//        /* xx.innerText = "Score =" + score;*/
+//        alert("Bravo Your Score is :   " + score + "     Press OK to move Easy Level");
+//        window.location = "EasyLevel.html";
+//    }
+//    if (score > 5 && score <= 7) {
+//        alert("Bravo Your Score is :   " + score + "     Press OK to move Intermediate Level");
+//        window.location = "IntermediateLevel.html";
+//    }
+//    if (score > 7) {
+//        alert("Bravo Your Score is :   " + score + "     Press OK to move Hard Level");
+//        /*window.Location = "HardLevel.html";*/
+//        window.open("HardLevel.html");
+//    }
+
+//};
+
+
+function myFunction() {
+    /*var xx = document.getElementById("hone");*/
+    /*   document.getElementById("hone").innerText = xx;*/
+    xx.innerText = "Score =" + score;
+}
+
+
+    function GoTest() {
+        window.location.href = "TestLevel.html";
+}
+
+
+
+                
